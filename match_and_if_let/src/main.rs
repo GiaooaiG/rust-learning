bafa2a0cead729de9032feeffa2b11d14663e11e
Gyh <3499847578@qq.com>
2024-05-05 17:02:51 +0800

    20240505

--- match_and_if_let/src/main.rs
+++ match_and_if_let/src/main.rs
@@ -1,3 +1,15 @@
+enum Coin {
+    1(i32),
+    0,
+    -1,
+}
+impl Coin {
+    fn print(&self) -> String {
+        
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let coin= Coin{
+
+    }
 }
