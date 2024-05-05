69b303830b8c096d7e44ee9e2f38bfe683377a31
Gyh <3499847578@qq.com>
2024-05-05 15:15:17 +0800

    20240505

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,8 +6,12 @@ impl Rectangle {
     fn area(&self) -> u32{
         self.width*self.length
     }
+    fn num() -> u32{
+        1
+    }
 }
 fn main(){
+    let a = Some(3);
     let rect = Rectangle{
         length:30,
         width:50,
@@ -16,8 +20,14 @@ fn main(){
     for i in  (1..4).rev(){
         println!("{}",i);
     }
+    println!("{}",Rectangle::num());
+    let is_ok = Ok::Yes;
+    match is_ok {
+        Ok::Yes => println!("OK"),
+        _ => println!("No"),
+    };
 }
-enum _Ok{
+enum Ok{
     Yes,
     No,
 }
