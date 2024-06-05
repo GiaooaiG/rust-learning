89cb2525a1ac20d09397dbe1aa96f9bd2e59b175
Gyh <3499847578@qq.com>
2024-06-05 21:43:39 +0800

    20240605

--- crates/calculator/src/main.rs
+++ crates/calculator/src/main.rs
@@ -1,11 +1,8 @@
+use std::cmp::Ordering;
 use std::env::{self, Args};
 use std::process;
-use std::cmp::Ordering;
 
-
-fn main() {
-    
-}
+fn main() {}
 
 enum Element {
     Number(f64),
@@ -21,17 +18,17 @@ enum Operator {
     Divide,
 }
 impl Operator {
-    fn get_priority(&self) -> u16{
-        match self{
+    fn get_priority(&self) -> u16 {
+        match self {
             Operator::Plus | Operator::Minus => 1,
             Operator::Multiply | Operator::Divide => 2,
             _ => 0,
         }
     }
 }
-    
-fn parse(input: &str) -> Result<Element,&'static str>{
-    match input{
+
+fn parse(input: &str) -> Result<Element, &'static str> {
+    match input {
         "(" => return Ok(Element::Operator(Operator::LeftBracket)),
         ")" => return Ok(Element::Operator(Operator::RightBracket)),
         "+" => return Ok(Element::Operator(Operator::Plus)),
@@ -45,50 +42,52 @@ fn parse(input: &str) -> Result<Element,&'static str>{
         Err(_) => return Err("Error"),
     };
 }
-fn convert_to_vec(args:Args) -> Vec<Element> {
-    let mut elements:Vec<Element> = Vec::new();
-    for i in args{
+fn convert_to_vec(args: Args) -> Vec<Element> {
+    let mut elements: Vec<Element> = Vec::new();
+    for i in args {
         elements.push(parse(&i).unwrap_or_else(|err| {
             eprintln!("Problem parsing arguments: {}", err);
-            process::exit(1)}));
+            process::exit(1)
+        }));
     }
     elements
 }
-fn run(elements:Vec<Element>) -> Vec<Element> {
-    let mut reverse_polish_notation:Vec<Element> = Vec::new();
-    let mut operators:Vec<Operator> = Vec::new();
-    for element in elements{
+fn run(elements: Vec<Element>) -> Vec<Element> {
+    let mut reverse_polish_notation: Vec<Element> = Vec::new();
+    let mut operators: Vec<Operator> = Vec::new();
+    for element in elements {
         match element {
             Element::Number(_) => reverse_polish_notation.push(i),
-            Element::Operator(operator) =>
-                match operator {
-                    Operator::LeftBracket => operators.push(operator),
-                    Operator::RightBracket => loop {
-                        match operators.pop() {
-                            Some(operator) => reverse_polish_notation.push(Element::Operator(operator)),
-                            None | Some(Operator::LeftBracket) => break,
-                        }
-                    },
-                    other_operator => match operators.last().unwrap().get_priority().cmp(&other_operator.get_priority()) { 
-                        Ordering::Greater => operators.push(other_operator),
-                        _ => ,
-                    },
+            Element::Operator(operator) => match operator {
+                Operator::LeftBracket => operators.push(operator),
+                Operator::RightBracket => loop {
+                    match operators.pop() {
+                        Some(operator) => reverse_polish_notation.push(Element::Operator(operator)),
+                        None | Some(Operator::LeftBracket) => break,
                     }
-            }
+                },
+                other_operator => match operators
+                    .last()
+                    .unwrap()
+                    .get_priority()
+                    .cmp(&other_operator.get_priority())
+                {
+                    Ordering::Greater => operators.push(other_operator),
+                    _ => (),
+                },
+            },
         }
+    }
     reverse_polish_notation
 }
 
 trait Stack<T> {
     fn top(&self) -> &T;
-    fn push(t:T) -> ();
-
+    fn push(t: T) -> ();
 }
-impl<T> Stack<T> for Vec<T>{
+impl<T> Stack<T> for Vec<T> {
     fn top(&self) -> &T {
         return self.last().unwrap();
     }
-    fn push(t:T) -> () {
-        
-    }
+    fn push(t: T) -> () {}
 }
--- crates/library/src/main.rs
+++ crates/library/src/main.rs
@@ -1,21 +1,87 @@
 use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world!");
+    run();
 }
-pub struct 
-pub struct Reader{
+pub struct Reader {
     name: String,
     tel_number: u64,
     is_vip: bool,
 }
+enum Choice {
+    Exit,
+    Register,
+}
 impl Reader {
-    pub fn new(name:String,tel_number:u64,is_vip:bool) -> Reader{
-        Reader{name,tel_number,is_vip}
+    pub fn new(name: String, tel_number: u64, is_vip: bool) -> Reader {
+        Reader {
+            name,
+            tel_number,
+            is_vip,
+        }
     }
 }
-fn run(){
-    let readers:HashMap<u64,Reader> = HashMap::new();
+fn run() {
+    let mut readers: HashMap<u64, Reader> = HashMap::new();
+    loop {
+        match get_choice() {
+            None => println!("非法输入!"),
+            Some(choice) => match choice {
+                Choice::Exit => break,
+                Choice::Register => register(&mut readers),
+            },
+        };
+    }
 }
-fn add_reader(reader:Reader,hashmap: &mut HashMap<u64,Reader>){
+fn add_reader(reader: Reader, hashmap: &mut HashMap<u64, Reader>) {
+    hashmap.insert(reader.tel_number, reader);
+}
+use std::io;
+fn get_choice() -> Option<Choice> {
+    println!("选择你的操作:");
+    println!("0: 退出");
+    println!("1: 读者注册");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error Reading Line");
+    match input.trim() {
+        "0" => Some(Choice::Exit),
+        "1" => Some(Choice::Register),
+        _ => None,
+    }
+}
+fn register(hashmap: &mut HashMap<u64, Reader>) {
+    let name: String;
+    let tel_number: u64;
+    let is_vip: bool;
+    let mut input = String::new();
+    println!("输入姓名:");
+    input.clear();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error Reading Line");
+    name = input.trim().parse().unwrap();
+    println!("输入电话:");
+    input.clear();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error Reading Line");
+    tel_number = input.trim().parse().unwrap();
+    println!("注册会员吗[y/n](n):");
+    input.clear();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error Reading Line");
+    is_vip = match input.as_str() {
+        "y" => true,
+        "n" => false,
+        _ => false,
+    };
+    let reader = Reader {
+        name,
+        tel_number,
+        is_vip,
+    };
+    add_reader(reader, hashmap)
 }
